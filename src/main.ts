2bc2957f42e33e464da69b5ed18a0339db53055a
matiaslgarcia <matiasgarcia444@gmail.com>
2024-04-29 11:40:30 -0300

    product ms finished

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,18 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const logger = new Logger('Main')
 
+  console.log(envs.NATS_SERVERS)
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port
+        servers: envs.NATS_SERVERS
       }
     }
-
   )
-  
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -54,7 +55,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const product = await this.product.findUnique({
       where: { id, available: true }
     })
-    if (!product) throw new NotFoundException(`Product with id #${id} not found`)
+    if (!product) throw new RpcException({
+      message: `Product with id #${id} not found`,
+      status: HttpStatus.BAD_REQUEST
+    })
 
     return product
   }
@@ -81,5 +85,27 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     })
 
     return product
+  }
+
+
+
+  async validateProducts(ids: number[]) {
+
+    ids = Array.from(new Set(ids))
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    })
+
+    if (products.length !== ids.length) throw new RpcException({
+      message: 'Some products were not found',
+      status: HttpStatus.BAD_REQUEST
+    })
+
+    return products
   }
 }
